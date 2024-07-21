80d268de515ecd217db1a22f0f47a53c25d22b0e
copeve1015 <copeve1015@gmail.com>
2024-07-22 05:21:25 +0900

    update 5 files, create 1 file and copy 1 file

---
+++ routes/admin.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+const logger = require('../lib/logger');
+const CustomError = require('../error/CustomError');
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const authRouter = require('./auth')
+const userRouter = require('./user')
+const adminRouter = require('./admin')
 
 router.get('/', function (req, res, next) {
   res.send('Hello World!');
 });
 
 router.use('/auth', authRouter)
+router.use('/users', userRouter)
+router.use('/admin', adminRouter)
 
 module.exports = router;
