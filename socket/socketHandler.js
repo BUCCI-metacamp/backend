3f970dd060e3fe37bb3c74dc308a004f72770534
copeve1015 <copeve1015@gmail.com>
2024-07-23 19:04:42 +0900

    update 3 files and create 1 file

---
+++ socket/socketHandler.js
@@ -0,0 +1,49 @@
+const tokenUtil = require("../lib/tokenUtil");
+
+let ioInstance;
+
+const socketHandler = (io) => {
+  ioInstance = io;
+
+  io.use((socket, next) => {
+    if (socket.handshake.query && socket.handshake.query.token) {
+      const loginToken = socket.handshake.query.token;
+      try {
+        console.log(loginToken);
+        // 토큰 유효한지 확인
+        const token = loginToken.split('Bearer ')[1];
+        const decoded = tokenUtil.verifyToken(token);
+
+        socket.decoded = decoded;
+        next();
+      } catch (error) {
+        return next(new Error('Authentication error'));
+      }
+    } else {
+      next(new Error('Authentication error'));
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log('새로운 클라이언트가 연결되었습니다.');
+
+    // 방에 연결
+    socket.on('request_join_room', (roomName) => {
+      socket.join(roomName);
+    });
+
+    // 연결 해제
+    socket.on('disconnect', () => {
+      console.log('클라이언트가 연결을 끊었습니다.');
+    });
+
+  });
+}
+
+socketHandler.emitToRoom = (room, event, data) => {
+  if (ioInstance) {
+    ioInstance.to(room).emit(event, data);
+  }
+};
+
+module.exports = socketHandler;
