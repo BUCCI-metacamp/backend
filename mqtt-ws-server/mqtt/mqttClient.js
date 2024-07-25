375036eed711488028028f2d43b02afd86ef7e0c
copeve1015 <copeve1015@gmail.com>
2024-07-25 17:15:13 +0900

    docker middle

--- mqtt-ws-server/mqtt/mqttClient.js
+++ mqtt-ws-server/mqtt/mqttClient.js
@@ -1,9 +1,28 @@
-// mqttClient.js
 const mqtt = require('mqtt');
+const awsIot = require('aws-iot-device-sdk');
+const fs = require('fs');
 const logger = require('../lib/logger');
 const { statusDataReceivedHandler, powerDataReceivedHandler } = require('../service/mqttService');
 
-const client = mqtt.connect('mqtt://192.168.0.64:1883');
+const privateKey = fs.readFileSync('/run/secrets/aws_iot_private_key', 'utf8');
+const certificate = fs.readFileSync('/run/secrets/aws_iot_certificate', 'utf8');
+const rootCA = fs.readFileSync('/run/secrets/aws_iot_root_ca', 'utf8');
+
+const client = awsIot.device({
+  privateKey: Buffer.from(privateKey),
+  clientCert: Buffer.from(certificate),
+  caCert: Buffer.from(rootCA),
+  clientId: process.env.AWS_IOT_CLIENT_ID,
+  host: process.env.AWS_IOT_ENDPOINT
+});
+
+// const client = awsIot.device({
+//   keyPath: process.env.AWS_IOT_PRIVATE_KEY_PATH,
+//   certPath: process.env.AWS_IOT_CERTIFICATE_PATH,
+//   caPath: process.env.AWS_IOT_ROOT_CA_PATH,
+//   clientId: process.env.AWS_IOT_CLIENT_ID,
+//   host: process.env.AWS_IOT_ENDPOINT
+// });
 
 client.on('connect', () => {
   logger.info('MQTT client connected');
@@ -21,8 +40,12 @@ client.on('message', async (topic, message) => {
       powerDataReceivedHandler(JSON.parse(messageString));
       break;
     default:
-      console.warn(`처리되지 않은 토픽: ${topic}`);
+      logger.info(`처리되지 않은 토픽: ${topic}`);
   }
+});
+
+client.on('error', function(error) {
+  logger.error('MQTT client Error:', error);
 });
 
 module.exports = client;
